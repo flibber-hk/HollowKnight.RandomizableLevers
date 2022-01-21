0b9c3cc2d4cee622f077fa11795a1da60380a392
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-01-21 00:32:59 +0000

    Fix waterways deployer removal

--- RandomizableLevers/Rando/LateRandoChanges.cs
+++ RandomizableLevers/Rando/LateRandoChanges.cs
@@ -17,7 +17,17 @@ namespace RandomizableLevers.Rando
             if (RandoInterop.Settings.RandomizeLevers && RandomizerMod.RandomizerMod.RS.GenerationSettings is not null)
             {
                 // Remove platforms in Waterways_04 in case they arrive there from the Waterways exit gate
-                ItemChanger.Internal.Ref.Settings.Deployers.RemoveAll(dep => dep is SmallPlatform sp && sp.SceneName == SceneNames.Waterways_04);
+                List<IDeployer> deployers = ItemChanger.Internal.Ref.Settings.Deployers;
+
+                foreach (IDeployer deployer in deployers)
+                {
+                    if (deployer is SmallPlatform sp && sp.SceneName == SceneNames.Waterways_04)
+                    {
+                        Events.RemoveSceneChangeEdit(deployer.SceneName, deployer.OnSceneChange);
+                    }
+                }
+
+                deployers.RemoveAll(dep => dep is SmallPlatform sp && sp.SceneName == SceneNames.Waterways_04);
             }
         }
     }
