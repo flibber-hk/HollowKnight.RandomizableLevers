ef82539ce33b05b58d62b49f64e2a434aec9e571
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-01-30 13:50:34 +0000

    Add levers to the condensed spoiler, and bump version

--- RandomizableLevers/Rando/RandoInterop.cs
+++ RandomizableLevers/Rando/RandoInterop.cs
@@ -1,4 +1,7 @@
-﻿namespace RandomizableLevers.Rando
+﻿using System.Collections.Generic;
+using RandomizerMod.Logging;
+
+namespace RandomizableLevers.Rando
 {
     internal static class RandoInterop
     {
@@ -12,7 +15,14 @@
             LateRandoChanges.Hook();
 
             RandomizerMod.Logging.SettingsLog.AfterLogSettings += AddLeverRandoSettings;
+
+            // Add important levers to the condensed spoiler log
+            CondensedSpoilerLogger.AddCategory("Important Levers", (args) => true, ImportantLevers);
+            CondensedSpoilerLogger.AddCategory("Palace Levers", (args) => true, PalaceLevers);
         }
+
+        private static bool ShouldLogPalaceLevers(LogArguments args)
+            => Settings.RandomizeLevers && args.gs.LongLocationSettings.WhitePalaceRando != RandomizerMod.Settings.LongLocationSettings.WPSetting.ExcludeWhitePalace;
 
         private static void AddLeverRandoSettings(RandomizerMod.Logging.LogArguments args, System.IO.TextWriter tw)
         {
@@ -21,5 +31,29 @@
             RandomizerMod.RandomizerData.JsonUtil._js.Serialize(jtw, Settings);
             tw.WriteLine();
         }
+
+        private static readonly List<string> ImportantLevers = new()
+        {
+            LeverNames.Switch_Dirtmouth_Stag,
+            LeverNames.Lever_Resting_Grounds_Stag,
+            LeverNames.Lever_Mantis_Claw,
+            LeverNames.Lever_Mantis_Lords_Access,
+            LeverNames.Lever_Sanctum_Soul_Warrior,
+            LeverNames.Lever_Sanctum_Bottom,
+            LeverNames.Lever_Failed_Tramway_Left,
+            LeverNames.Lever_Failed_Tramway_Right,
+            LeverNames.Lever_Queens_Gardens_Ground_Block,
+        };
+
+        private static readonly List<string> PalaceLevers = new()
+        {
+            LeverNames.Lever_Palace_Entrance_Orb,
+            LeverNames.Lever_Palace_Left_Orb,
+            LeverNames.Lever_Palace_Right_Orb,
+            LeverNames.Lever_Palace_Atrium,
+            LeverNames.Lever_Palace_Right,
+            LeverNames.Lever_Palace_Final,
+            LeverNames.Lever_Path_of_Pain,
+        };
     }
 }
