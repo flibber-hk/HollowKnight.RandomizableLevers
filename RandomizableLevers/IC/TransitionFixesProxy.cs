53bc40b70033383b7ce5e7ebbf7f610bd813a3ff
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-02-14 07:32:28 +0000

    Prevent transition fix in Fungus2_14

--- RandomizableLevers/IC/TransitionFixesProxy.cs
+++ RandomizableLevers/IC/TransitionFixesProxy.cs
@@ -54,6 +54,9 @@ namespace RandomizableLevers.IC
                     case SceneNames.Town when t.GateName == "door_station":
                         Finder.GetItem(LeverNames.Switch_Dirtmouth_Stag).GiveImmediate(null);
                         break;
+                    case SceneNames.Fungus2_14 when t.GateName.StartsWith("bot"):
+                        Finder.GetItem(LeverNames.Lever_Mantis_Lords_Access).GiveImmediate(null);
+                        break;
                 }
             }
         }
