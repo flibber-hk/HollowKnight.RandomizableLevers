524b95e943e6eeaaaede6e7cd7230e31d5ad6414
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-06-24 17:17:50 +0100

    LanguageData compatibility fix

--- RandomizableLevers/LanguageData.cs
+++ RandomizableLevers/LanguageData.cs
@@ -56,11 +56,13 @@ namespace RandomizableLevers
 
         private static string OverrideLanguageString(string key, string sheetTitle, string orig)
         {
-            // If orig has already been overridden, then it was probably an ItemChanger language override
-            if (orig != Language.Language.GetInternal(key, sheetTitle)) return orig;
-
-            LanguageKey obj = new(sheetTitle, key);
-            return LanguageStrings.TryGetValue(obj, out string overrideValue) ? overrideValue : orig;
+            if (key == null || sheetTitle == null) return orig;
+            if (string.IsNullOrEmpty(orig) || orig.StartsWith("#!"))
+            {
+                LanguageKey obj = new(sheetTitle, key);
+                return LanguageStrings.TryGetValue(obj, out string value) ? value : orig;
+            }
+            return orig; // If orig has already been overridden, then it was probably an ItemChanger language override
         }
     }
 }
